9356aa00453b2273eca43ebaa65e014c83cd93ab
vladivladi1177 <161205114+vladivladi1177@users.noreply.github.com>
2024-05-28 04:35:23 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+}
+    const data = generateRandomData();
+    const data = [];
+function generateRandomData() {
+        console.log("Random Number: " + item);
+}
+    for (let i = 0; i < 10; i++) {
+
+main();
+
+function main() {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    data.forEach(item => {
+    }
+    });function generateRandomData() {
+    });
+
+    data.forEach(item => {
+}
+        console.log("Random Number: " + item);
+    }
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+    const data = generateRandomData();
+function main() {
+main();
+    const data = [];
+
+    return data;
